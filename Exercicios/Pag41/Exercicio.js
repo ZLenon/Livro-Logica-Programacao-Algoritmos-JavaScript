92e446b1bd129a8b0d1c6f947c0bad390ae2db1c
Lenon <lenoninformatica@gmail.com>
2023-01-01 15:43:44 -0300

    Exercicio pag41

---
+++ Exercicios/Pag41/Exercicio.js
@@ -0,0 +1,10 @@
+/* Elaborar um programa que leia dois números. Calcule e informe a soma
+desses números.
+• Entrada de dados: ler dois números
+• Processamento: calcular a soma
+• Saída: informar a soma */
+
+function calcSoma(num1, num2) {
+  return num1 + num2;
+};
+console.log(calcSoma(5,5));
