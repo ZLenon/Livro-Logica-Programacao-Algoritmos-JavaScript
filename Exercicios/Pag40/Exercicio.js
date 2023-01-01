9a5814c7140349a7326867f0c7629331b95352cf
Lenon <lenoninformatica@gmail.com>
2023-01-01 15:37:03 -0300

    Exercicio pag40

---
+++ Exercicios/Pag40/Exercicio.js
@@ -0,0 +1,11 @@
+/* Elaborar um programa que leia um número. Calcule e informe o dobro
+desse número. 
+Entrada de dados: 
+Ler um número
+• Processamento: calcular o dobro
+• Saída: informar o dobro */
+
+function calcDouble(num) {
+  return num * 2;
+};
+console.log(calcDouble(10));
