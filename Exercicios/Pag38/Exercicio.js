f5a98cb9a4b2a0ce901907ef012af5f0bbe1e782
Lenon <lenoninformatica@gmail.com>
2023-01-01 15:15:57 -0300

    Exercicio pag38

---
+++ Exercicios/Pag38/Exercicio.js
@@ -0,0 +1,12 @@
+// Tipos de dados e conversões de tipos
+
+var a = "20";
+var b = a * 2; // b = 40;
+var c = a / 2; // c = 10;
+var d = a - 2; // d = 18
+var e = a + 2; // e = 202 nesse utimo foi concatenado 20 e 2
+
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
