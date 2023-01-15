dc44f5d31ab4402ca9a6be826c1e08ac0a65717a
Lenon <lenoninformatica@gmail.com>
2023-01-15 15:05:31 -0300

    Teste Pag82

--- Tests/Pag82.test.js
+++ Tests/Pag82.test.js
@@ -1,13 +1,10 @@
 //import { expect } from '@jest/globals';
 const { expect } = require('@jest/globals')
-
+const { botton, addEventListener } = require('../Exercicios/Pag82/script');
 
 describe('Pagina 82',()=>{
   it('tentanto se ha um inpult com o id Name',()=>{
-    const nameAluno = document.getElementById('name');
-    const notaUm = document.getElementById('nota1');
-    const notaDois = document.getElementById('nota2');
-    const botton = document.getElementById('btn');
-    expect(nameAluno).toBe('title')
+
+    expect(addEventListener).toBe('QualquerCoisa')
   });
 });
