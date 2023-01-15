8620112a98922159ef30b9f23ea266d1eaff50d9
Lenon <lenoninformatica@gmail.com>
2023-01-14 22:48:38 -0300

    Exercicio 02

--- ExerciciosAdicionais/02/script.js
+++ ExerciciosAdicionais/02/script.js
@@ -1,1 +1,51 @@
 /* https://www.alura.com.br/artigos/alterando-css-com-javascript  */
+
+function mudaCor(td) {
+  const dataPagina = td.textContent;
+  const hoje = new Date ();
+  const data = new Date( dataPagina );
+  const quantidadeDeDias = calculoDeDias(hoje, data);
+
+  if (quantidadeDeDias < 10){
+    mudaParaVermelho(td);
+  }
+  else if (quantidadeDeDias < 15){
+    mudaParaAmarelo(td);
+  }
+  else
+   mudaParaVerde(td);
+  };
+};
+
+function mudaParaVermelho(td) {
+  td.style.backgroundColor = "#FF0F0F";
+}
+
+function mudaParaAmarelo(td) {
+  td.style.backgroundColor = "#FFFA00";
+}
+
+function mudaParaVerde(td) {
+  td.style.backgroundColor = "#31FF4E";
+}
+
+function calculoDeDias(dataAtual, ultimaRevisao){
+  const segundos = ( dataAtual - ultimaRevisao ) / 1000 ;
+  const horas = ( milesimos / 3600 );
+  const dias = ( horas / 24 );
+  const quantidadeDeDias = Math.floor(dias);
+
+  return quantidadeDeDias;
+
+  };
+
+  function verifica(ultimaRevisoes){
+    ultimaRevisoes.forEach(td => {
+      mudaCor(td);
+    });
+  }
+
+  document.addEventListener("DOMContentLoaded", function(){    
+    const proximasRevisoes = document.querySelectorAll(".info-proximaRevisao");
+    verifica( proximasRevisoes );
+  }
