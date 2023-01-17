749980fd5fd42222bfbc87708e7d9af56fdaacf7
Lenon <lenoninformatica@gmail.com>
2023-01-16 22:44:35 -0300

    Exercicio Pag89

---
+++ Exercicios/Pag89/script.js
@@ -0,0 +1,12 @@
+function calc() {
+  const nameInput = document.getElementById('hadleName');
+  const genderM = document.getElementById('genderMasculino');
+  const genderF = document.getElementById('genderFeminino');
+  const altura =  document.getElementById('altura');
+  const btnCalPesos = document.getElementById('calPesos');
+  const btnClearInput = document.getElementById('clearInput');
+  const spanPeso = document.getElementById('pesoIdeal');
+
+
+
+}
