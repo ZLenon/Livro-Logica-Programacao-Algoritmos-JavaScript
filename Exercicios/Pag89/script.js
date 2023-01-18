24e23de7e00d187ca26a9ab63048c3a02ecf1995
Lenon <lenoninformatica@gmail.com>
2023-01-17 21:35:47 -0300

    Exercicio Pag89

--- Exercicios/Pag89/script.js
+++ Exercicios/Pag89/script.js
@@ -1,12 +1,37 @@
-function calc() {
+const btnCalPesos = document.getElementById('calPesos');
+const btnClearInput = document.getElementById('clearInput');
+
+function calcPeso() {
   const nameInput = document.getElementById('hadleName');
   const genderM = document.getElementById('genderMasculino');
   const genderF = document.getElementById('genderFeminino');
-  const altura =  document.getElementById('altura');
-  const btnCalPesos = document.getElementById('calPesos');
-  const btnClearInput = document.getElementById('clearInput');
+  const inAltura =  document.getElementById('altura');
   const spanPeso = document.getElementById('pesoIdeal');
 
+  const name = nameInput.value;
+  const masculino = genderM.checked;
+  const feminino = genderF.checked;
+  const altura = Number(inAltura.value);
+
+  if (name === '' || (masculino === false && feminino === false)) {
+    alert('Por favor, informe um nome e selecione um sexo...');
+    nameInput.focus(); 
+    return;
+  }
+  if (altura === 0 || inNaN(altura)) {
+    alert('Por favor, informe a altura corretamente...');
+    altura.focus();
+    return;
+  }
+  if (masculino) {
+    const peso = 22 * Math.pow(altura, 2);
+    return spanPeso.textContent = `${nome}: seu peso ideal é ${peso.toFixed(3)}Kg`;
+  } else {
+    const peso = 21 * Math.pow(altura, 2);
+    return spanPeso.textContent = `${nome}: seu peso ideal é ${peso.toFixed(3)}Kg`;
+
+  }
 
 
 }
+btnCalPesos.addEventListener('click', calcPeso );
